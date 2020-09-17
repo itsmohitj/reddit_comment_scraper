4a1ae974673bb6daa344f20d36783d73e1122834
itsmohitj <itsmohitj@gmail.com>
2020-09-17 11:41:10 +0530

    Added the script for finding words in Top 50 Comments

--- reddit_comment_scraper.py
+++ reddit_comment_scraper.py
@@ -18,3 +18,30 @@ commonWords = {'that','this','and','of','the','for','I','it','has','in',
 'at','about','would','like','there','You','from','get','just','more','so',
 'me','more','out','up','some','will','how','one','what',"don't",'should',
 'could','did','no','know','were','did',"it's",'This','he','The','we'}
+
+for submission in subreddit.top(limit=50):
+    submission.comments.replace_more(limit=0)
+    for top_level_comment in submission.comments:
+        count += 1
+        if(count == max):
+            break
+        word = ""
+        for letter in top_level_comment.body:
+            if(letter == ' '):
+                if(word and not word[-1].isalnum()):
+                    word = word[:-1]
+                if not word in commonWords:
+                    words.append(word)
+                word = ""
+            else:
+                word += letter
+    if(count == max):
+            break
+
+for word in words:
+    if word in wordCount:
+        wordCount[word] += 1
+    else:
+        wordCount[word] = 1
+
+print(wordCount)
