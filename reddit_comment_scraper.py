faff10d28c2518d0654ea26e84e663bb56d0837e
itsmohitj <itsmohitj@gmail.com>
2020-09-17 14:04:30 +0530

    Added Plot Using Pandas and Seaborn

--- reddit_comment_scraper.py
+++ reddit_comment_scraper.py
@@ -1,5 +1,7 @@
 import praw
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
 reddit = praw.Reddit(client_id='client_id', client_secret='Client_Secret', user_agent='comment_scraper', username='Reddit_Username', password='Reddit_Password')
 
@@ -7,20 +9,20 @@ subreddit_name = input("Enter the name of the subreddit")
 subreddit = reddit.subreddit(subreddit_name)
 top_subbreddit = subreddit.top()
 count = 0
-max = 10000
-print('success')
+max = 1000
 words = []
 wordCount = {}
-commonWords = {'that','this','and','of','the','for','I','it','has','in',
-                 'you','to','was','but','have','they','a','is','','be','on','are','an','or',
-                 'at','as','do','if','your','not','can','my','their','them','they','with',
-                 'at','about','would','like','there','You','from','get','just','more','so',
-                 'me','more','out','up','some','will','how','one','what',"don't",'should',
-                 'could','did','no','know','were','did',"it's",'This','he','The','we',
-                 'all','when','had','see','his','him','who','by','her','she','our','thing','-',
-                 'now','what','going','been','we',"I'm",'than','any','because','We','even',
-                 'said','only','want','other','into','He','what','i','That','thought',
-                 'think',"that's",'Is','much'}
+commonWords = {'these','they','that','this','and','of','the','for','I','it','has','in',
+                'you','to','was','but','have','they','a','is','','be','on','are','an','or',
+                'at','as','do','if','your','not','can','my','their','them','they','with',
+                'at','about','would','like','there','You','from','get','just','more','so',
+                'me','more','out','up','some','will','how','one','what',"don't",'should',
+                'could','did','no','know','were','did',"it's",'This','he','The','we',
+                'all','when','had','see','his','him','who','by','her','she','our','thing','-',
+                'now','what','going','been','we',"I'm",'than','any','because','We','even',
+                'said','only','want','other','into','He','what','i','That','thought',
+                'think',"that's",'Is','much'}
+
 for submission in subreddit.top(limit=50):
     submission.comments.replace_more(limit=0)
     for top_level_comment in submission.comments:
@@ -46,8 +48,6 @@ for word in words:
     else:
         wordCount[word] = 1
 
-print(wordCount)
-
 sortedList = sorted(wordCount, key = wordCount.get, reverse = True)
 
 keyWords = []
@@ -61,7 +61,17 @@ for entry in sortedList:
     if (amount == 10):
         break
 
-plt.title('Top comments for: r/' + subreddit_name)
-plt.pie(keyCount, labels=keyWords, autopct='%1.1f%%',shadow=True, startangle=90)
-plt.axis('equal') 
+labels = keyWords
+sizes = keyCount
+
+#Uncomment to plot using MatPlotLib
+#plt.title('Top comments for: r/' + subreddit_name)
+#plt.pie(sizes, labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+#plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+#plt.show()
+
+
+sns.set_palette('magma')
+series = pd.Series(sizes, index = labels, name = "Plot")
+series.plot.pie(subplots=True, autopct="%.2f", figsize=(10,6))
 plt.show()
