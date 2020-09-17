8a39cde74a547c532120c87755d1cff8f4c60871
itsmohitj <itsmohitj@gmail.com>
2020-09-17 11:33:18 +0530

    Added Common Words

--- reddit_comment_scraper.py
+++ reddit_comment_scraper.py
@@ -6,4 +6,15 @@ reddit = praw.Reddit(client_id='client_id', client_secret='Client_Secret', user_
 
 subreddit_name = "learnpython"
 subreddit = reddit.subreddit(subreddit_name)
-
+top_subbreddit = subreddit.top()
+count = 0
+max = 10000
+print('success')
+words = []
+wordCount = {}
+commonWords = {'that','this','and','of','the','for','I','it','has','in',
+'you','to','was','but','have','they','a','is','','be','on','are','an','or',
+'at','as','do','if','your','not','can','my','their','them','they','with',
+'at','about','would','like','there','You','from','get','just','more','so',
+'me','more','out','up','some','will','how','one','what',"don't",'should',
+'could','did','no','know','were','did',"it's",'This','he','The','we'}
